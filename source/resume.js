88ca4531a4b75ac77457c1b3fe5816adac178c24
Amin Sadeghi <amin.sadeghi@live.com>
2024-05-27 12:59:53 -0400

    chore: Add last updated section to resume page via JS

--- source/resume.js
+++ source/resume.js
@@ -1,6 +1,26 @@
-var ol_tags = document.getElementsByTagName('ol')
+/**
+ * Adds a last updated section to the document body.
+ */
+function addLastUpdatedSection() {
+    var hr = document.createElement('hr');
+    hr.style.height = '0.5px';
+    hr.style.border = 'none';
+    hr.style.backgroundColor = 'lightgray';
+    hr.classList.add('fade-rule');
 
-for (var i=0, max=ol_tags.length; i < max; i++) {
-    ol_tags[i].setAttribute('reversed', 'reversed')
+    var br1 = document.createElement('br');
+    var br2 = document.createElement('br');
+
+    var p = document.createElement('p');
+    p.className = 'center';
+    var em = document.createElement('em');
+    em.textContent = 'Last updated: ' + new Date().toISOString().split('T')[0];
+    p.appendChild(em);
+
+    document.body.appendChild(br1);
+    document.body.appendChild(br2);
+    document.body.appendChild(hr);
+    document.body.appendChild(p);
 }
 
+addLastUpdatedSection();
